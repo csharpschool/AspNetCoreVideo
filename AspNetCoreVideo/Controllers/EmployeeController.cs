e12c31040be8340df8f69615839de878a323bb81
Jonas <N/A>
2017-04-16 16:34:46 +0200

    Added a new controller named EmployeeController

---
+++ AspNetCoreVideo/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿namespace AspNetCoreVideo.Controllers
+{
+    public class EmployeeController
+    {
+        public string Name()
+        {
+            return "Jonas";
+        }
+
+        public string Country()
+        {
+            return "Sweden";
+        }
+
+        public string Index()
+        {
+            return "Hello from Employee";
+        }
+
+    }
+}
