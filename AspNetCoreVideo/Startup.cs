a29fecd14846e2faa5de1aaef191426a6fb32239
Jonas <N/A>
2017-04-16 15:38:29 +0200

    Read text from appsettings.json in the Configure method in the Startup class

--- AspNetCoreVideo/Startup.cs
+++ AspNetCoreVideo/Startup.cs
@@ -1,17 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace AspNetCoreVideo
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -30,7 +38,8 @@ namespace AspNetCoreVideo
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello, from My World!");
+                var message = Configuration["Message"];
+                await context.Response.WriteAsync(message);
             });
         }
     }
