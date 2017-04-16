7fb82d0a9e807c932b0deb148cb532663b9a2dfb
Jonas <N/A>
2017-04-16 19:19:39 +0200

    Added the VideoDbContext class

---
+++ AspNetCoreVideo/Data/VideoDbContext.cs
@@ -0,0 +1,23 @@
+﻿using AspNetCoreVideo.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreVideo.Data
+{
+    public class VideoDbContext : DbContext
+    {
+        public DbSet<Video> Videos { get; set; }
+
+        public VideoDbContext(DbContextOptions<VideoDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
