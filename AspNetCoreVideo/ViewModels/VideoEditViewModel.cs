7fef017ae03c0f8fbc5859279dbcdeaee3ebe9f7
Jonas <N/A>
2017-04-16 18:48:15 +0200

    Added the VideoEditViewModel class

---
+++ AspNetCoreVideo/ViewModels/VideoEditViewModel.cs
@@ -0,0 +1,11 @@
+﻿using AspNetCoreVideo.Models;
+
+namespace AspNetCoreVideo.ViewModels
+{
+    public class VideoEditViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Genres Genre { get; set; }
+    }
+}
