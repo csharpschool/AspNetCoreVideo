5bfe1100856dce3e9c9eefb6cac87c462d745d52
Jonas <N/A>
2017-04-16 16:37:05 +0200

    Route attributes with hadrcoded sting route names

--- AspNetCoreVideo/Controllers/EmployeeController.cs
+++ AspNetCoreVideo/Controllers/EmployeeController.cs
@@ -1,17 +1,23 @@
-﻿namespace AspNetCoreVideo.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreVideo.Controllers
 {
+    [Route("employee")]
     public class EmployeeController
     {
+        [Route("name")]
         public string Name()
         {
             return "Jonas";
         }
 
+        [Route("country")]
         public string Country()
         {
             return "Sweden";
         }
 
+        [Route("")]
         public string Index()
         {
             return "Hello from Employee";
