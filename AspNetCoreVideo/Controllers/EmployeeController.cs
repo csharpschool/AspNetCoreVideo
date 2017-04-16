fd49e44d1698aa350aa900a4d8ee5c731d61e9d1
Jonas <N/A>
2017-04-16 16:40:08 +0200

    Generic route names on class level and no attributes on teh actions

--- AspNetCoreVideo/Controllers/EmployeeController.cs
+++ AspNetCoreVideo/Controllers/EmployeeController.cs
@@ -2,22 +2,19 @@
 
 namespace AspNetCoreVideo.Controllers
 {
-    [Route("[controller]")]
+    [Route("company/[controller]/[action]")]
     public class EmployeeController
     {
-        [Route("[action]")]
         public string Name()
         {
             return "Jonas";
         }
 
-        [Route("[action]")]
         public string Country()
         {
             return "Sweden";
         }
 
-        [Route("[action]")]
         public string Index()
         {
             return "Hello from Employee";
