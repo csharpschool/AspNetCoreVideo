a268bab52a069fcb4e8bd9f9ab3385546fb5d64a
Jonas <N/A>
2017-04-17 10:33:57 +0200

    Added a View Component for the IMessageService service

---
+++ AspNetCoreVideo/ViewComponents/Message.cs
@@ -0,0 +1,26 @@
+﻿using AspNetCoreVideo.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreVideo.ViewComponents
+{
+    public class Message : ViewComponent
+    {
+        private IMessageService _message;
+
+        public Message(IMessageService message)
+        {
+            _message = message;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _message.GetMessage();
+            return View("Default", model);
+        }
+
+    }
+}
