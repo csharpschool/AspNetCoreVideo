b9ce5e96e1f3f4daa49fb6d42caae479be6bb368
Jonas <N/A>
2017-04-17 10:18:36 +0200

    Added the Edit view and its actions

--- AspNetCoreVideo/Controllers/HomeController.cs
+++ AspNetCoreVideo/Controllers/HomeController.cs
@@ -69,6 +69,31 @@ namespace AspNetCoreVideo.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var video = _videos.Get(id);
+
+            if (video == null) return RedirectToAction("Index");
+
+            return View(video);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, VideoEditViewModel model)
+        {
+            var video = _videos.Get(id);
+
+            if (video == null || !ModelState.IsValid) return View(model);
+
+            video.Title = model.Title;
+            video.Genre = model.Genre;
+
+            _videos.Commit();
+
+            return RedirectToAction("Details", new { id = video.Id });
+        }
+
 
     }
 }
