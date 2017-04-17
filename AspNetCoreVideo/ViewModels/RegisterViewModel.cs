f384933695536bfce44fee6dd1259ba70ccb8617
Jonas <N/A>
2017-04-17 10:55:55 +0200

    Added user registration: AccountController, RegisterViewModel, Register view and actions

---
+++ AspNetCoreVideo/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreVideo.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required, MaxLength(255)]
+        public string Username { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password), Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}
