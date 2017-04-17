9ec0818c471d7271cb7fc049c562c4c11487193c
Jonas <N/A>
2017-04-17 11:08:07 +0200

    Added the HTTP POST Login Action

--- AspNetCoreVideo/Controllers/AccountController.cs
+++ AspNetCoreVideo/Controllers/AccountController.cs
@@ -62,5 +62,25 @@ namespace AspNetCoreVideo.Controllers
             return View(model);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, false);
+            if (result.Succeeded)
+            {
+                if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                {
+                    return Redirect(model.ReturnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            ModelState.AddModelError("", "Login failed");
+            return View(model);
+        }
+
     }
 }
