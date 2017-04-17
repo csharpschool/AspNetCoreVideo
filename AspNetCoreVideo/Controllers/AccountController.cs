524bb7648cd8fbb9148a293553472cd6af5c9acb
Jonas <N/A>
2017-04-17 11:02:49 +0200

    Added the Logout Action

--- AspNetCoreVideo/Controllers/AccountController.cs
+++ AspNetCoreVideo/Controllers/AccountController.cs
@@ -48,5 +48,13 @@ namespace AspNetCoreVideo.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+
     }
 }
