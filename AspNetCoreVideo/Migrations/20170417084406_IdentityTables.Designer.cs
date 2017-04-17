4808955c20bd2c4e64c7538e7bbc84111664003d
Jonas <N/A>
2017-04-17 10:45:01 +0200

    Creating the AspNet Identity Database Tables

--- AspNetCoreVideo/Migrations/20170417084406_IdentityTables.Designer.cs
+++ AspNetCoreVideo/Migrations/20170417084406_IdentityTables.Designer.cs
@@ -9,9 +9,10 @@ using AspNetCoreVideo.Models;
 namespace AspNetCoreVideo.Migrations
 {
     [DbContext(typeof(VideoDbContext))]
-    partial class VideoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170417084406_IdentityTables")]
+    partial class IdentityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.1")
